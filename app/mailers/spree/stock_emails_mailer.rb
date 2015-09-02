3c201e9c4ab7a479d0218b8cd55334f7bbd8136e
Viraj Chaudhari <viraj@thingbits.net>
2015-09-02 15:15:19 +0530

    Inherit from Spree::BaseMailer

--- app/mailers/spree/stock_emails_mailer.rb
+++ app/mailers/spree/stock_emails_mailer.rb
@@ -1,5 +1,5 @@
 module Spree
-  class StockEmailsMailer < ActionMailer::Base
+  class StockEmailsMailer < Spree::BaseMailer
     default from: Spree::StockEmailConfig::Config.email_from
 
     def stock_email(stock_email)
