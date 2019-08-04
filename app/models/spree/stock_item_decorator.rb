8d4c8ec62a0de973cbde21171e21857f9b25f93e
virajrch <virajz.c@gmail.com>
2019-08-04 18:30:26 +0530

    User product instead of variant

--- app/models/spree/stock_item_decorator.rb
+++ app/models/spree/stock_item_decorator.rb
@@ -2,6 +2,6 @@ Spree::StockItem.class_eval do
   after_save :send_stock_emails
 
   def send_stock_emails
-    Spree::StockEmail.notify(variant) if variant.in_stock?
+    Spree::StockEmail.notify(product) if product.in_stock?
   end
 end
