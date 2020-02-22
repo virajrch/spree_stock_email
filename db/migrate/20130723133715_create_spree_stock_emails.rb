3d6a648237877e57356a65d70a45201971eea7f7
virajrch <virajz.c@gmail.com>
2020-02-22 06:37:52 +0530

    Update migration version to 4.1

--- db/migrate/20130723133715_create_spree_stock_emails.rb
+++ db/migrate/20130723133715_create_spree_stock_emails.rb
@@ -1,4 +1,4 @@
-class CreateSpreeStockEmails < ActiveRecord::Migration
+class CreateSpreeStockEmails < ActiveRecord::Migration[4.1]
   def change
     create_table :spree_stock_emails do |t|
       t.timestamps
