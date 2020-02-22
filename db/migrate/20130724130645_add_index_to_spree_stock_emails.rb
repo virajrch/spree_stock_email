c11381b797151e3d5e72dc09c941f4993b42111a
virajrch <virajz.c@gmail.com>
2020-02-22 06:41:04 +0530

    Update version to 4.2

--- db/migrate/20130724130645_add_index_to_spree_stock_emails.rb
+++ db/migrate/20130724130645_add_index_to_spree_stock_emails.rb
@@ -1,4 +1,4 @@
-class AddIndexToSpreeStockEmails < ActiveRecord::Migration[4.1]
+class AddIndexToSpreeStockEmails < ActiveRecord::Migration[4.2]
   def change
     add_index :spree_stock_emails, [:sent_at, :variant_id, :email]
   end
